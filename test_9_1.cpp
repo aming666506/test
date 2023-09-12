6e05f724844ea2f2f0f5d1540a084810d843d726
aming666506 <144472343+aming666506@users.noreply.github.com>
2023-09-12 15:18:09 +0800

    Add files via upload

---
+++ test_9_1.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<cstdio>
+#include<iostream>
+#include<string>
+using namespace std;
+string s;
+string trim() 
+{
+
+}
+int main() 
+{
+	getline(cin,s);
+	string t = trim(s);
+	cout << t<< endl;
+	return 0;
+}
